d6dfdb1efb07e4cfb569110192672f64821e7842
Samantha Tran <stran@Samanthas-MacBook-Air.local>
2022-10-22 11:17:39 +1000

    umm

--- app/frontend/src/features/auth/AuthService.tsx
+++ app/frontend/src/features/auth/AuthService.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { UserType } from "../../types/UserType";
 
-const API_URL = "/api/users/";
-
+const API_URL = "api/users/";
 // Register user
 const register = async (userData: UserType) => {
   const response = await axios.post(API_URL + "register", userData);
